c40383b99969448b74f5d625580d24f41a1b9394
RanPAN <yuanlan8010@gmail.com>
2026-03-18 14:29:48 +0100

    finalisation de configuration le tomcat dans docker

--- src/main/java/com/qualieau/util/DBConnection.java
+++ src/main/java/com/qualieau/util/DBConnection.java
@@ -5,17 +5,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	private static final String URL = "jdbc:mysql://localhost:3306/qualieau_db?serverTimezone=UTC";
+    private static final String DOCKER_URL = "jdbc:mysql://db-mysql:3306/qualieau_db?serverTimezone=UTC";
+    private static final String LOCAL_URL = "jdbc:mysql://localhost:3306/qualieau_db?serverTimezone=UTC";
     private static final String USER = "root"; 
     private static final String PASSWORD = "root"; 
 
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+
+            try {
+                return DriverManager.getConnection(DOCKER_URL, USER, PASSWORD);
+            } catch (SQLException e) {
+                return DriverManager.getConnection(LOCAL_URL, USER, PASSWORD);
+            }
+            
         } catch (ClassNotFoundException e) {
             throw new SQLException("Pilote JDBC MySQL introuvable.", e);
         }
     }
-
 }
